5f828a1ddd69a7a14781d0a15e01c5c1f5ef64c2
JonathanSeguraXP <jonathansegura.cv@gmail.com>
2025-08-16 08:31:28 -0600

    parte 4

--- spring-ext-config/src/test/java/sv/edu/udb/configuration/EnvironmentDbConfigTest.java
+++ spring-ext-config/src/test/java/sv/edu/udb/configuration/EnvironmentDbConfigTest.java
@@ -3,6 +3,7 @@ package sv.edu.udb.configuration;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.EnabledIf;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
@@ -17,9 +18,24 @@ class EnvironmentDbConfigTest {
         assertNotNull(environmentDbConfig);
     }
     @Test
-    void shouldEnvironmentHasValue_When_SpringContextWork() {
+    @EnabledIf(expression = "#{environment['spring.profiles.active'] == 'dev'}",
+            loadContext = true)
+    void shouldEnvironmentHasValue_When_SpringDevContextWork() {
         final String dbDriverExpected = "org.h2.Driver";
-        final String dbUrlExpected = "jdbc:h2:mem:test";
+        final String dbUrlExpected = "jdbc:h2:mem:dev";
+        final String dbUsernameExpected = "SA";
+        final String dbPasswordExpected = "";
+        assertEquals(dbDriverExpected, environmentDbConfig.getDbDriver());
+        assertEquals(dbUrlExpected, environmentDbConfig.getDbUrl());
+        assertEquals(dbUsernameExpected, environmentDbConfig.getDbUsername());
+        assertEquals(dbPasswordExpected, environmentDbConfig.getDbPassword());
+    }
+    @Test
+    @EnabledIf(expression = "#{environment['spring.profiles.active'] == 'prod'}",
+            loadContext = true)
+    void shouldEnvironmentHasValue_When_SpringProdContextWork() {
+        final String dbDriverExpected = "org.h2.Driver";
+        final String dbUrlExpected = "jdbc:h2:mem:prod";
         final String dbUsernameExpected = "SA";
         final String dbPasswordExpected = "";
         assertEquals(dbDriverExpected, environmentDbConfig.getDbDriver());
